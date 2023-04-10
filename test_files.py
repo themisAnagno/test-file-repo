73431f0d7842f5d268fd42662de243865ff4f295
themisAnagno <them.anagno@gmail.com>
2023-04-10 14:32:06 +0300

    Updated Jenkinsfile

--- test_files.py
+++ test_files.py
@@ -5,3 +5,9 @@ panos_path = os.getenv("panos")
 
 print("Vaggelis path is {}".format(vaggelis_path))
 print("Panos path is {}".format(panos_path))
+
+with open(vaggelis_path, 'r') as vag:
+    print(vag.read())
+
+with open(panos_path, 'r') as p:
+    print(p.read())
